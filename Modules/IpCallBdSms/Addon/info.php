d15c29200c044dceaf97ba098ff7a124cc27cd33
Exonhost Server <farhanalmasud9208@gmail.com>
2026-09-09 22:55:01 +0600

    update 4.1 version

--- Modules/IpCallBdSms/Addon/info.php
+++ Modules/IpCallBdSms/Addon/info.php
@@ -1,7 +1,7 @@
 <?php return array (
   'software_id' => 'FC000002',
   'name' => 'IpCallBdSms',
-  'is_published' => 0,
+  'is_published' => 1,
   'purchase_code' => 'fc-ipcallbd-local',
   'username' => 'foodcollection',
 );
