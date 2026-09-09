f47741842e6ed210fa1b935dd130ebefe9199f5e
Exonhost Server <farhanalmasud9208@gmail.com>
2026-09-09 22:58:19 +0600

    update 4.1 version codecanyon

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,46 +48,9 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-
-            $namespace = $this->namespace;
-            $registerHostRoutes = function () use ($namespace) {
-                Route::middleware('web')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/web.php'));
-
-                Route::prefix('admin')
-                    ->middleware('web')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/admin.php'));
-
-                Route::prefix('vendor-panel')
-                    ->middleware('web')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/vendor.php'));
-
-                Route::prefix('api/v1')
-                    ->middleware('api')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/api/v1/api.php'));
-
-                Route::prefix('api/v2')
-                    ->middleware('api')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/api/v2/api.php'));
-
-                //new routes
-                Route::prefix('admin')
-                    ->middleware('web')
-                    ->namespace($namespace)
-                    ->group(base_path('routes/admin/routes.php'));
-            };
-
-            $hostDomain = config('app.host_domain');
-            if ($hostDomain) {
-                Route::domain($hostDomain)->group($registerHostRoutes);
-            } else {
-                $registerHostRoutes();
-            }
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/update.php'));
         });
     }
 
@@ -99,7 +62,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(600)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
